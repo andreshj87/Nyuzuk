75bd3afc206265d75767792dc49783349dbf32cf
Andrés Hernández <andreshj87@gmail.com>
2019-08-21 22:12:24 +0200

    Refactor Either creation in a more functional way

--- data/src/main/kotlin/com/andres/nyuzuk/data/datasource/ArticleRemoteDataSource.kt
+++ data/src/main/kotlin/com/andres/nyuzuk/data/datasource/ArticleRemoteDataSource.kt
@@ -1,31 +1,27 @@
 package com.andres.nyuzuk.data.datasource
 
 import arrow.core.Either
+import com.andres.nyuzuk.data.entity.ArticleRemote
 import com.andres.nyuzuk.data.entity.ArticleResponse
-import com.andres.nyuzuk.data.mapper.ArticleMapper
 import com.andres.nyuzuk.data.remote.ArticleApiService
 import com.andres.nyuzuk.domain.Failure
-import com.andres.nyuzuk.domain.entity.Article
 import retrofit2.Response
 
 class ArticleRemoteDataSource(
-    private val articleApiService: ArticleApiService,
-    private val articleMapper: ArticleMapper
+    private val articleApiService: ArticleApiService
 ) {
-    suspend fun getTopArticles(): Either<Failure, List<Article>> {
+    suspend fun getTopArticles(): Either<Failure, List<ArticleRemote>> {
         return fromResponse(articleApiService.getTopArticles())
     }
 
-    suspend fun searchArticles(query: String): Either<Failure, List<Article>> {
+    suspend fun searchArticles(query: String): Either<Failure, List<ArticleRemote>> {
         return fromResponse(articleApiService.searchArticles(query))
     }
 
-    private fun fromResponse(response: Response<ArticleResponse>): Either<Failure, List<Article>> {
-        return if (response.isSuccessful && response.body() != null) {
-            val articles = articleMapper.map(response.body()!!.articles)
-            Either.Right(articles)
-        } else {
-            Either.Left(Failure.ApiError)
-        }
+    private fun fromResponse(response: Response<ArticleResponse>): Either<Failure, List<ArticleRemote>> {
+        return Either.cond(
+            response.isSuccessful && response.body() != null,
+            { response.body()!!.articles },
+            { Failure.ApiError })
     }
 }
--- data/src/main/kotlin/com/andres/nyuzuk/data/repository/ArticleDataRepository.kt
+++ data/src/main/kotlin/com/andres/nyuzuk/data/repository/ArticleDataRepository.kt
@@ -1,12 +1,18 @@
 package com.andres.nyuzuk.data.repository
 
+import arrow.core.Either
+import arrow.core.flatMap
 import com.andres.nyuzuk.data.datasource.ArticleRemoteDataSource
+import com.andres.nyuzuk.data.mapper.ArticleMapper
 import com.andres.nyuzuk.domain.repository.ArticleRepository
 
 class ArticleDataRepository(
-    private val articleRemoteDataSource: ArticleRemoteDataSource
+    private val articleRemoteDataSource: ArticleRemoteDataSource,
+    private val articleMapper: ArticleMapper
 ) : ArticleRepository {
-    override suspend fun getTopArticles() = articleRemoteDataSource.getTopArticles()
+    override suspend fun getTopArticles() =
+        articleRemoteDataSource.getTopArticles().flatMap { Either.Right(articleMapper.map(it)) }
 
-    override suspend fun searchArticles(query: String) = articleRemoteDataSource.searchArticles(query)
+    override suspend fun searchArticles(query: String) =
+        articleRemoteDataSource.searchArticles(query).flatMap { Either.Right(articleMapper.map(it)) }
 }
