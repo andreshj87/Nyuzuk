d97b5752565e45b8edfc792fb178d932d4f5c884
Andrés Hernández <andreshj87@gmail.com>
2019-08-22 18:49:52 +0200

    Add Paginator class

--- data/src/main/kotlin/com/andres/nyuzuk/data/repository/ArticleDataRepository.kt
+++ data/src/main/kotlin/com/andres/nyuzuk/data/repository/ArticleDataRepository.kt
@@ -4,14 +4,28 @@ import arrow.core.Either
 import arrow.core.flatMap
 import com.andres.nyuzuk.data.datasource.ArticleRemoteDataSource
 import com.andres.nyuzuk.data.mapper.ArticleMapper
+import com.andres.nyuzuk.domain.Failure
+import com.andres.nyuzuk.domain.entity.Article
 import com.andres.nyuzuk.domain.repository.ArticleRepository
 
 class ArticleDataRepository(
     private val articleRemoteDataSource: ArticleRemoteDataSource,
     private val articleMapper: ArticleMapper
 ) : ArticleRepository {
-    override suspend fun getTopArticles() =
-        articleRemoteDataSource.getTopArticles().flatMap { Either.Right(articleMapper.map(it)) }
+    private val topArticlesPaginator = Paginator()
+
+    override suspend fun getTopArticles(): Either<Failure, List<Article>> {
+        return if (topArticlesPaginator.shouldContinue()) {
+            articleRemoteDataSource.getTopArticles()
+                .flatMap {
+                    topArticlesPaginator.updatePage()
+                    return Either.Right(articleMapper.map(it))
+                }
+        } else {
+            Either.Left(Failure.ApiError)
+        }
+
+    }
 
     override suspend fun searchArticles(query: String) =
         articleRemoteDataSource.searchArticles(query).flatMap { Either.Right(articleMapper.map(it)) }
