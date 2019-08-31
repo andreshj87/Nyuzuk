06382824355db46cc27f5b1f38a4b1ae4210f154
Andrés Hernández <andreshj87@gmail.com>
2019-08-31 12:18:31 +0200

    Add some unit tests for domain entities

--- domain/src/test/kotlin/com/andres/nyuzuk/domain/entity/ArticleTest.kt
+++ domain/src/test/kotlin/com/andres/nyuzuk/domain/entity/ArticleTest.kt
@@ -1,15 +1,60 @@
 package com.andres.nyuzuk.domain.entity
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
 import org.junit.Test
-import kotlin.test.assertEquals
+import java.util.*
 
 class ArticleTest {
+    companion object {
+        private const val SOME_TITLE = "Hurricane Dorian heads for the US"
+        private const val SOME_DESCRIPTION =
+            "The storm is currently a Category 2 storm heading northwest in the Atlantic. Follow here for the latest."
+        private const val SOME_CONTENT =
+            "As Dorian barrels towards the island chain on Saturday, a hurricane warning has been issued in areas in northwest Bahamas, according to an alert issued by the Bahamas Department of Meteorology on Friday."
+        private const val SOME_AUTHOR = "Lee Moran"
+        private val SOME_PUBLISHER = PublisherTest.SOME_PUBLISHER
+        private const val SOME_IMAGE_URL = ""
+        private const val SOME_URL = ""
+        private val SOME_PUBLISHED_AT = Date()
+        val SOME_ARTICLE = Article(
+            SOME_TITLE,
+            SOME_DESCRIPTION,
+            SOME_CONTENT,
+            SOME_AUTHOR,
+            SOME_PUBLISHER,
+            SOME_IMAGE_URL,
+            SOME_URL,
+            SOME_PUBLISHED_AT
+        )
+    }
+
     private lateinit var article: Article
 
     @Test
     fun `should create`() {
-        article = Article("title", "description", "content", "author", null, "imageUrl", "url")
+        article = Article(
+            SOME_TITLE,
+            SOME_DESCRIPTION,
+            SOME_CONTENT,
+            SOME_AUTHOR,
+            SOME_PUBLISHER,
+            SOME_IMAGE_URL,
+            SOME_URL,
+            SOME_PUBLISHED_AT
+        )
 
-        assertEquals("title", article.title)
+        assertThat(article).isNotNull()
+        article.run {
+            assertThat(title).isEqualTo(SOME_TITLE)
+            assertThat(description).isEqualTo(SOME_DESCRIPTION)
+            assertThat(content).isEqualTo(SOME_CONTENT)
+            assertThat(author).isEqualTo(SOME_AUTHOR)
+            assertThat(publisher).isEqualTo(SOME_PUBLISHER)
+            assertThat(imageUrl).isEqualTo(SOME_IMAGE_URL)
+            assertThat(url).isEqualTo(SOME_URL)
+            assertThat(publishedAt).isEqualTo(SOME_PUBLISHED_AT)
+        }
     }
 }
---
+++ domain/src/test/kotlin/com/andres/nyuzuk/domain/entity/PublisherTest.kt
@@ -0,0 +1,39 @@
+package com.andres.nyuzuk.domain.entity
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
+import assertk.assertions.isNull
+import org.junit.Test
+
+class PublisherTest {
+    companion object {
+        private const val SOME_ID = "fox-news"
+        private const val SOME_NAME = "Fox news"
+        val SOME_PUBLISHER = Publisher(SOME_ID, SOME_NAME)
+    }
+
+    private lateinit var publisher: Publisher
+
+    @Test
+    internal fun `should create`() {
+        publisher = Publisher(SOME_ID, SOME_NAME)
+
+        assertThat(publisher).isNotNull()
+        publisher!!.run {
+            assertThat(id).isEqualTo(SOME_ID)
+            assertThat(name).isEqualTo(SOME_NAME)
+        }
+    }
+
+    @Test
+    fun `should create with null id`() {
+        publisher = Publisher(null, SOME_NAME)
+
+        assertThat(publisher).isNotNull()
+        publisher.run {
+            assertThat(id).isNull()
+            assertThat(name).isEqualTo(SOME_NAME)
+        }
+    }
+}
