839e88384b87c295c5b8d318c86e06cc4acd6858
Andrés Hernández <andreshj87@gmail.com>
2019-08-31 11:38:50 +0200

    Add assertK

--- presentation/src/test/kotlin/com/andres/nyuzuk/presentation/base/PublisherUiMapperTest.kt
+++ presentation/src/test/kotlin/com/andres/nyuzuk/presentation/base/PublisherUiMapperTest.kt
@@ -1,5 +1,6 @@
 package com.andres.nyuzuk.presentation.base
 
+import com.andres.nyuzuk.presentation.mapper.PublisherUiMapper
 import org.hamcrest.core.IsNull.nullValue
 import org.junit.Assert.assertThat
 import org.junit.Before
@@ -14,7 +15,7 @@ class PublisherUiMapperTest {
     }
 
     @Test
-    fun `should get null when mapping null Publisher`() {
+    fun `should get null when mapping null publisher`() {
         val publisherUi = publisherUiMapper.map(null)
 
         assertThat(publisherUi, nullValue())
